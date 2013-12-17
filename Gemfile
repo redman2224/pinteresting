383dbc044857abaf981bd18dc803492399c12c85
Dan Redmond <dan.w.redmond@gmail.com>
2013-12-16 21:40:08 -0500

    add devise

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+#gem 'ruby', '2.0.0'
 gem 'rails', '4.0.0'
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
@@ -8,6 +9,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass'
+gem "devise", "~> 3.2.2"
 
 group :doc do
   gem 'sdoc', require: false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  #required for heroku
+  #note to set this to actual hostname
+  config.action_mailer.default_url_options = { :host => 'http://still-peak-5126.herokuapp.com/' }
 end
