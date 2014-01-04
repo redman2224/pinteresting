8e7230298995323baa0268711d0ba37b925d7715
Dan Redmond <dan.w.redmond@gmail.com>
2014-01-04 11:30:40 -0500

    add user name and validations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
+	validates :image, :description, presence: true
+
 end
